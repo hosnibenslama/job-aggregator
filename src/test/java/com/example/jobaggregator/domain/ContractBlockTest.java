4d01e276f60b8e5557567ae41433f7c68d77ba9e
hosnibs <hosniibs@gmail.com>
2026-09-05 21:35:14 +0200

    test: rename HierarchyDomainModelTest to ContractBlockTest

--- src/test/java/com/example/jobaggregator/domain/ContractBlockTest.java
+++ src/test/java/com/example/jobaggregator/domain/ContractBlockTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
-class HierarchyDomainModelTest {
+class ContractBlockTest {
 
     @Test
     void shouldCreateArticleWithFullComponents() {
